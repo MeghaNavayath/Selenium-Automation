ee1178509dec93a4fd03f580882256d20802b1bf
MeghaNavayath <megha.navayath@gmail.com>
2026-03-16 10:02:21 +0530

    Update Mavenproject2.java

--- src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject2;
 public class Mavenproject2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("Hello World!");
     }
 }
