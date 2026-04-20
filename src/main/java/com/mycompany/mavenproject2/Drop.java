8361b35b2d5f2c04eab920b7f28269ef3408293b
MeghaNavayath <megha.navayath@gmail.com>
2026-04-20 09:43:24 +0530

    Update Drop.java

--- src/main/java/com/mycompany/mavenproject2/Drop.java
+++ src/main/java/com/mycompany/mavenproject2/Drop.java
@@ -19,7 +19,7 @@ public class Drop
        Select select = new Select (dd);
        select.selectByVisibleText("Option 2");
        String newd = select.getFirstSelectedOption().getText();
-       System.out.println("Selected option was : +newd");
+       System.out.println("Selected option were : +newd");
        driver.quit();
        
         
